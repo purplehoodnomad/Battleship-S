9ff9f67afb7322667cabb7748bacee92e71ff91c
purplehoodnomad <pragmatismwontdie@gmail.com>
2025-10-08 19:49:26 +0500

    Add: better type annotations Refactor: simplified planet setup method

--- modules/field.py
+++ modules/field.py
@@ -18,7 +18,16 @@ class Field:
         self._cells = {}
         self.dimensions = {"height": 0, "width": 0}
         self.shape = "empty"
-        if shape is not None or params is not None: self.generate_field(shape, params)
+        if shape is not None or params is not None:
+            self.generate_field(shape, params)
+
+    @property
+    def useful_cells_coords(self) -> list[tuple[int, int]]:
+        """
+        Returns list of all non-void cells.
+        Optimizes data transfering by removing useless information.
+        """
+        return [coords for coords, cell in self._cells.items() if not cell.is_void]
 
 
     def is_empty(self) -> bool:
@@ -31,7 +40,7 @@ class Field:
         logger.info(f"{self} wiped")
 
 
-    def cell_exists(self, coords: tuple) -> bool:
+    def cell_exists(self, coords: tuple[int, int]) -> bool:
         """
         Checks if cell with given (y, x) is part of field
         """
@@ -39,7 +48,7 @@ class Field:
         return coords in self._cells
     
 
-    def generate_field(self, shape: str|int, params: list) -> None:
+    def generate_field(self, shape: str|int, params: list[int]) -> None:
         """
         Gets list of shape and generation parameters (width, height, radius etc.)
         This is separate method and not constructor because
@@ -115,7 +124,7 @@ class Field:
                 raise FieldException(f"{self}: no {shape} shape supported")
 
 
-    def vodify_corners(self, coords: list) -> None:
+    def vodify_corners(self, coords: list[tuple[int, int]]) -> None:
         """
         Expects list of edge coords
         Makes void all cells which must be vodified to form a field shape
@@ -189,22 +198,22 @@ class Field:
         self.vodify_corners(normalized_coords)
 
 
-    def get_cell(self, coords: tuple) -> Cell:
+    def get_cell(self, coords: tuple[int, int]) -> Cell:
         """
         This method only returns cell instance by given (y, x) if it part of the field.
         It can return void cell and returns no cell if field is empty.
         It's made to separate responsibility - it just gives what it can.
         """
         if self.is_empty():
-            logger.warning(f"{self}: Tried to get cell with no field.")
-            return
+            raise FieldException(f"{self}: Tried to get cell with no field.")
+        
         if not coords: raise FieldException(f"{self}: Asked for no cells.")
         
         try: return self._cells[coords]
         except KeyError: raise FieldException(f"{self}: Requested Cell {coords} does not exist.")
     
 
-    def occupy_cells(self, entity, anchor_coords: tuple, rotation: int) -> None:
+    def occupy_cells(self, entity, anchor_coords: tuple[int, int], rotation: int) -> None:
         """
         Takes entity and tries to place it correspondingly given (y, x) and rotation values.
         Raises FieldException if can't by any reason.
@@ -213,6 +222,8 @@ class Field:
         """
         if entity is None: raise FieldException(f"{self}: entity can't be None.")
         if anchor_coords is None: raise FieldException(f"{self}: anchor_coords can't be None.")
+        if entity.type == EntityType.PLANET:
+            raise FieldException("Tried to place planet as regular entity. Use setup_a_planet instead")
 
         # asks which cells entity wants to take depending on it's properties
         # recieves list of (y,x) and correct rotation (e.g.  rot = 5  -->  rot = 1)
@@ -241,7 +252,7 @@ class Field:
         entity.update_state(anchor_coords = anchor_coords, occupied_cells = reserved_coords, rotation = rotation, status = EntityStatus.FULLHEALTH)
 
 
-    def neighbours(self, coord_list: list) -> set:
+    def neighbours(self, coord_list: list[tuple[int, int]]) -> set[tuple[int, int]]:
             """
             Returns all potential coordinates of cells near the entity
             """
@@ -259,63 +270,57 @@ class Field:
     
 
     def setup_a_planet(self, planet):
-        if planet is None: raise FieldException(f"{self}: planet can't be None.")
+        """
+        Places planet on the field.
+        Can be placed out of bounds, but orbit must cross field at least once.
+        """
+        if planet.type != EntityType.PLANET:
+            raise FieldException(f"Tried to setup a planet {planet} which is {planet.type}. Use occupy cells instead")
         
         orbit_cells = set()
         real_cells_counter = 0
         for coords in planet.orbit_cells:
             try:
                 cell = self.get_cell(coords)
-            except FieldException: continue
+            except FieldException:
+                continue # when tried to place orbit cell out of bounds - skip
             
-            if cell.occupied_by is not None:
-                raise FieldException(f"{self}: orbit crosses {coords} is already occupied by {cell.occupied_by}")
             orbit_cells.add(coords)
-            
             if not cell.is_void:
                 real_cells_counter += 1
         
         if real_cells_counter == 0: raise FieldException(f"{self}: orbit of planet never crosses any field cell. Change center or radius")
         for coords in orbit_cells:
             self.get_cell(coords).occupied_by = planet
-        planet.update_state(occupied_cells=list(orbit_cells),status=EntityStatus.FULLHEALTH)
+        planet.update_state(occupied_cells=list(orbit_cells),status=EntityStatus.DAMAGED)
 
     
-    def take_shot(self, coords) -> CellStatus:
+    def take_shot(self, coords: tuple[int, int]) -> CellStatus:
         """
-        Returns result of attempt: miss, hit or destroyed
-        If ship is destroyed - makes all nearby cells was_shot = True
-        This provides instant update to the field
+        Returns result of attempt.
         """
         cell = self.get_cell(coords)
-        if cell.is_void or cell.was_shot: raise FieldException(f"{self}: {coords} is not valid target")
-        
-        if cell.occupied_by is None:
-            cell.was_shot = True
-            logger.info(f"{self}: {cell} was shot")
-            return CellStatus.MISS
+        if cell.is_void or cell.was_shot:
+            raise FieldException(f"{self}: {coords} is not valid target")
         
         cell.was_shot = True
+        
+        if cell.occupied_by is None:
+            return CellStatus.MISS
+        
         occupator = cell.occupied_by
-
         if occupator.type == EntityType.PLANET:
             if coords == occupator.anchor:
                 return CellStatus.HIT
             else:
                 return CellStatus.MISS
         
-        elif occupator.type == EntityType.RELAY and occupator.status != EntityStatus.DESTROYED:
+        elif occupator.type == EntityType.RELAY:
             occupator.make_damage(coords)
             return CellStatus.RELAY
         
         else:
             occupator.make_damage(coords)
-            if occupator.status == EntityStatus.DESTROYED:
-                # filling all nearby as was_shot
-                for close_coords in self.neighbours(occupator.cells_occupied):
-                    self.get_cell(close_coords).was_shot = True
-                    logger.info(f"{self}: {close_coords} marked as shot next to destroyed entity")
-                return CellStatus.DESTROYED
             return CellStatus.HIT
         
 
@@ -339,23 +344,14 @@ class Cell:
         self.is_void = is_void
         self.was_shot = False
         self.occupied_by = None
+
+    def free(self) -> None:
+        self.occupied_by = None
     
     def __str__(self):
         if self.is_void:
             return f"void({self.y},{self.x})"
-        
-        if self.occupied_by is None:
-            state = "o" if self.was_shot else "."
-        elif self.occupied_by == EntityType.PLANET:
-            state = "c"
-        else:
-            state = "x" if self.was_shot else "■"
-            
-        return f"cell({self.y},{self.x},{state})"
+        return f"cell({self.y},{self.x})"
     
     def __repr__(self):
         return f"Cell({self.y},{self.x}) is_void={self.is_void}, was_shot={self.was_shot}"
-
-    def free(self):
-        self.occupied_by = None
-        logger.debug(f"{self} state updated")

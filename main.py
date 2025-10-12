bef997e18f9e7fa4c0fbc95344eb38f95639174f
purplehoodnomad <pragmatismwontdie@gmail.com>
2025-10-12 17:42:16 +0500

    Add: logging directory and log filename with date

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from cli.cli_io import main
+from datetime import datetime
+import os
 import logging
-with open("log.log", "w") as file: file.write("")
+from cli.cli_io import main
+
+
+logs_dir = "logs"
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)
+
+current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_filename = f"{logs_dir}/{current_time}.log"
+
+
 logging.basicConfig(
-filename = "log.log",
-level = logging.DEBUG,
-format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-filemode = "a",
+    filename=log_filename,
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filemode="a",
+    encoding="UTF-8"
 )
+
 
 main()
